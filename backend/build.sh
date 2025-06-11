e8142ad54eeab7e0d484fbf721b7abb6bbe45ed7
null <null>
2025-06-11 16:45:18 -0400

    notes from spin hackathon

--- backend/build.sh
+++ backend/build.sh
@@ -1,1 +1,8 @@
-docker build -t fastapi_example .
+#docker build -t fastapi_example .
+
+# new docker build command below, original old above
+
+# registry/project/namespace/image_name:tag <--- namespace is different than spin namespace
+# registry.nersc.gov has a web interface; the namespace can be modified there
+
+docker build -t registry.nersc.gov/atom/ips_fastran_test/fastapi_example:latest .
